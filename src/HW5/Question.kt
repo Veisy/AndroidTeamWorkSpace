0d8774ad9c42317ca0e95f499fcff59e871db4b2
VeisyLocal <knutveyshamsun@gmail.com>
2021-05-29 22:51:31 +0300

    Comment update.

--- src/HW5/Question.kt
+++ src/HW5/Question.kt
@@ -3,8 +3,11 @@ package HW5
 class Question(
         val question: String,
         val correctAnswer: String,
+        // We use this 'wrongAnswers' only in this class,
+        // so we make it parameter (it is not property anymore) by deleting 'val'.
         wrongAnswers: Array<String>) {
 
+    // TODO: Pay attention to '*' (Spread Operator) before 'wrongAnswers' array.
     val options: Array<String> = arrayOf(correctAnswer, *wrongAnswers)
 
     // TODO: What is this 'init' ?
