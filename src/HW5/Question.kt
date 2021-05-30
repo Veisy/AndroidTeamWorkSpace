31d7d2b547fcae1293c466344ff12b88d06339ec
VeisyLocal <knutveyshamsun@gmail.com>
2021-05-30 22:58:15 +0300

    HW5 solution.

--- src/HW5/Question.kt
+++ src/HW5/Question.kt
@@ -1,23 +1,27 @@
 package HW5
 
 class Question(
-        val question: String,
-        val correctAnswer: String,
-        // We use this 'wrongAnswers' only in this class,
-        // so we make it parameter (it is not property anymore) by deleting 'val'.
+        question: String,
+        correctAnswer: String,
         wrongAnswers: Array<String>) {
 
+    // Trim all String parameters. (Remove white spaces from the start and the end of the Strings)
+    val question = question.trim()
+    val correctAnswer = correctAnswer.trim()
+
     // TODO: Pay attention to '*' (Spread Operator) before 'wrongAnswers' array.
-    val options: Array<String> = arrayOf(correctAnswer, *wrongAnswers)
+    val options: Array<String> = arrayOf(this.correctAnswer, *wrongAnswers)
 
     // TODO: What is this 'init' ?
     init {
+        for (index in options.indices) options[index] = options[index].trim()
+
         //TODO: What is this 'shuffle' doing?
         options.shuffle()
     }
 
     fun printOptions() {
-        for(index in options.indices) {
+        for (index in options.indices) {
             println("${index + 1}) ${options[index]}")
         }
     }
--- src/HW5/main.kt
+++ src/HW5/main.kt
@@ -2,34 +2,31 @@ package HW5
 
 fun main() {
 
-    val question1 = Question(
-            "What is your name?",
-            "Yusuf",
-            arrayOf("Ahmet", "Selim", "Hakan"))
+    val allQuestions = getQuestions()
+    allQuestions.shuffle()
 
-    // TODO: Why this 'repeat' variable is declared with 'var' instead of 'val'?
-    // TODO: You wont use 'while' loop in your exercise, only 'for' loop to iterate all questions.
-    var repeat = true
-    while (repeat) {
+    // TODO: Why this 'correctAnswerCounter' variable is declared with 'var' instead of 'val' ?
+    var correctAnswerCounter = 0
 
-        println("\nTo quit, press Q\n")
-        println("\t\t\t ${question1.question}")
-        question1.printOptions()
+    for (question in allQuestions) {
 
-        val answer = readLine()
+        println("\n${question.question}")
+        question.printOptions()
+
+        var answer = readLine()
 
         if (!answer.isNullOrEmpty()) {
+
+            answer = answer.trim()
 
             // TODO: Pay attention to this point. We can use the if statement as an expression.
             // TODO: We can use everything as expressions in Kotlin.
             // User can enter answer as choice number or actual String.
-            val message = if (answer == "q" || answer == "Q") {
-                repeat = false
-                "Program terminated. See you later."
-            } else if (
+            val message = if (
             // TODO: What is this 'ignoreCase' doing?
-                    answer.equals(question1.correctAnswer, ignoreCase = true) ||
-                    answer == (question1.options.indexOf(question1.correctAnswer) + 1).toString()) {
+                    answer.equals(question.correctAnswer, ignoreCase = true) ||
+                    answer == (question.options.indexOf(question.correctAnswer) + 1).toString()) {
+                correctAnswerCounter++
                 "That is true."
             } else {
                 "Ohh, wrong answer!"
@@ -39,5 +36,34 @@ fun main() {
             println(message)
         }
     }
+    println("\nOverall, you got $correctAnswerCounter out of ${allQuestions.size} correct.\n" +
+            "Thanks for playing!")
+
 }
+
+fun getQuestions(): Array<Question> = arrayOf(
+        Question(
+                "How old is the Universe? (in billion years)",
+                "13.8",
+                arrayOf("0.77", "5.6", "2.2")),
+        Question(
+                "How old is Planet Earth? (in billion years)",
+                "4.5",
+                arrayOf("3.7", "15.5", "450")),
+        Question(
+                "How many years ago did the meteor that killed the dinosaurs hit the earth? (in million years)",
+                "66",
+                arrayOf("42", "181", "265")),
+
+        Question(
+                "What is the scientific name of human being?",
+                "Homo sapiens",
+                arrayOf("Homo erectus", "Homo neanderthalensis", "Homo habilis")),
+        Question(
+                "What is the most distant human-made object?",
+                "Voyager 1",
+                arrayOf("Voyager 2", "ISS", "Helios Satellites"))
+)
+
+
 
